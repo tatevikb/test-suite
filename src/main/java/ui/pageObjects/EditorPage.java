2804d87fd36a57ea4e4660fcffb5a3ed9f19be6f
tatev.badalyan <tatevikm.badalyan@gmail.com>
2022-11-21 10:58:02 +0400

    minor changes

--- src/main/java/ui/pageObjects/EditorPage.java
+++ src/main/java/ui/pageObjects/EditorPage.java
@@ -31,5 +31,7 @@ public class EditorPage extends DriverFactory {
 
     public void checkEditorLoaded(){
         wait.until(ExpectedConditions.urlContains("?category=layout&customSize=1080x1080&unit=px"));
+        layoutBtn.isSelected();
     }
+
 }
